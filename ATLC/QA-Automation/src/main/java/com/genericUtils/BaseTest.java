2ca8b8709566e47c99d4d9b356b8ede4ba5f65b8
PreviseTest <testrenregression@gmail.com>
2022-06-22 16:08:37 +0530

    Chrome driver updated to 103 version

--- ATLC/QA-Automation/src/main/java/com/genericUtils/BaseTest.java
+++ ATLC/QA-Automation/src/main/java/com/genericUtils/BaseTest.java
@@ -39,59 +39,70 @@ public class BaseTest {
     public static  LoginPage loginPage;
     @BeforeTest
     public void preconditions() throws IOException {
-        prop=new Properties();
-        String filepath1=System.getProperty("user.dir")+"/src/test/resources/config/project.properties";
-        fileInput = new FileInputStream(filepath1);
-        prop.load(fileInput);
-        System.setProperty(CHROME_DRIVER_KEY, CHROME_DRIVER_PATH);
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.setPageLoadStrategy(PageLoadStrategy.NONE);
-        // Instantiate the chrome driver
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT,TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
-        wait=new WebDriverWait(driver,(EXPLICIT_WAIT));
-        driver.manage().window().maximize();
-        builder=new Actions(driver);
-        js = (JavascriptExecutor) driver;
-        extent = new ExtentReports(System.getProperty("user.dir") + "/Execution Status/Extent Reports/"+"ATLC".concat("_"+BasePage.currentDateAndTime())+".html", true);
-        extent.addSystemInfo("Executed By ", prop.getProperty("userName"));
-        extent.addSystemInfo("Environment", prop.getProperty("url"));
-        extent.loadConfig(new File(System.getProperty("user.dir") + "/Execution Status/Extent Reports/"+"ATLC".concat("_"+BasePage.currentDateAndTime())+ ".html"));
-        driver.get(prop.getProperty("url"));
-        loginPage = new LoginPage(driver);
+    	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Prati\\git\\LinuxTest\\ATLC\\QA-Automation\\src\\test\\resources\\chromedriver.exe");
+		ChromeOptions Options = new ChromeOptions();
+		Options.setHeadless(true);
+		 driver = new RemoteWebDriver(new URL("http://18.212.190.211:4445/"));
+		driver.get(" https\\://test.porting.com/\\#/Login");
+		driver.getTitle();
+		System.out.println("Title of URL is "+ driver.getTitle());
     }
-    @AfterMethod(alwaysRun = true)
-    public void afterTestMethod(ITestResult res) throws IOException {
-        if (res.getStatus() == ITestResult.SUCCESS) {
-            logger.log(LogStatus.PASS, res.getTestClass().getName() + " is Passed ");
-        } else if (res.getStatus() == ITestResult.FAILURE) {
-            String name = res.getName();
-            System.out.println(name+" test class name");
-            File Path = BasePage.captureScreenshot(driver, name + " " + BasePage.currentDateAndTime());
-            String screenshotPath = Path.getPath();
-            screenshotPath = screenshotPath.replace(System.getProperty("user.dir")+"/Execution Status/ScreenShots/"+"ATLC"+"/", "");
-            System.out.println(screenshotPath);
-            logger.log(LogStatus.FAIL, "" + res.getThrowable());
-            logger.log(LogStatus.FAIL, logger.addScreenCapture(screenshotPath));
-            logger.log(LogStatus.FAIL, res.getTestClass().getName() + " is Failed ");
-        } else if (res.getStatus() == ITestResult.SKIP) {
-            logger.log(LogStatus.SKIP, res.getTestClass().getName() + " is Skipped");
-        }
-        extent.endTest(logger);
-
-    }
-    @AfterTest
-    public void postConditions() throws InterruptedException {
-        extent.flush();
-        extent.close();
-        /*Logout method should call*/
-        Thread.sleep(10000);
-//        loginPage.logOutSteps();
-//        driver.close();
-
-    }
+//        prop=new Properties();
+//        String filepath1=System.getProperty("user.dir")+"/src/test/resources/config/project.properties";
+//        fileInput = new FileInputStream(filepath1);
+//        prop.load(fileInput);
+//        System.setProperty(CHROME_DRIVER_KEY, CHROME_DRIVER_PATH);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("headless");
+//        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+//        // Instantiate the chrome driver
+//        //driver = new RemoteWebDriver(new URL("http://18.212.190.211:4445/"));
+//        driver = new ChromeDriver(options);
+//        driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT,TimeUnit.SECONDS);
+//        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+//        
+//        wait=new WebDriverWait(driver,(EXPLICIT_WAIT));
+//        driver.manage().window().maximize();
+//        builder=new Actions(driver);
+//        js = (JavascriptExecutor) driver;
+//        extent = new ExtentReports(System.getProperty("user.dir") + "/Execution Status/Extent Reports/"+"ATLC".concat("_"+BasePage.currentDateAndTime())+".html", true);
+//        extent.addSystemInfo("Executed By ", prop.getProperty("userName"));
+//        extent.addSystemInfo("Environment", prop.getProperty("url"));
+//        extent.loadConfig(new File(System.getProperty("user.dir") + "/Execution Status/Extent Reports/"+"ATLC".concat("_"+BasePage.currentDateAndTime())+ ".html"));
+//        driver.get(prop.getProperty("url"));
+//        loginPage = new LoginPage(driver);
+//    }
+//    @AfterMethod(alwaysRun = true)
+//    public void afterTestMethod(ITestResult res) throws IOException {
+//        if (res.getStatus() == ITestResult.SUCCESS) {
+//            logger.log(LogStatus.PASS, res.getTestClass().getName() + " is Passed ");
+//        } else if (res.getStatus() == ITestResult.FAILURE) {
+//            String name = res.getName();
+//            System.out.println(name+" test class name");
+//            File Path = BasePage.captureScreenshot(driver, name + " " + BasePage.currentDateAndTime());
+//            String screenshotPath = Path.getPath();
+//            screenshotPath = screenshotPath.replace(System.getProperty("user.dir")+"/Execution Status/ScreenShots/"+"ATLC"+"/", "");
+//            System.out.println(screenshotPath);
+//            logger.log(LogStatus.FAIL, "" + res.getThrowable());
+//            logger.log(LogStatus.FAIL, logger.addScreenCapture(screenshotPath));
+//            logger.log(LogStatus.FAIL, res.getTestClass().getName() + " is Failed ");
+//        } else if (res.getStatus() == ITestResult.SKIP) {
+//            logger.log(LogStatus.SKIP, res.getTestClass().getName() + " is Skipped");
+//        }
+//        extent.endTest(logger);
+//
+//    }
+//    @AfterTest
+//    public void postConditions() throws InterruptedException {
+//        extent.flush();
+//        extent.close();
+//        /*Logout method should call*/
+//        Thread.sleep(10000);
+////        loginPage.logOutSteps();
+////        driver.close();
+//
+//    }
 
 }
 
